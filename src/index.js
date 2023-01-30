8254cfcf3ec0bf9ee798ccbadd08deb9daa05a49
Brent <brenthickman@ymail.com>
2023-01-30 15:04:14 -0800

    updated display
    Co-authored-by: johnedisc <christopher.johnedis@gmail.com>
    Co-Authored-By: Zookerman3 <szook7@gmail.com>

--- src/index.js
+++ src/index.js
@@ -9,11 +9,19 @@ function handleBlogEntry(event) {
   event.preventDefault();
   const titleEntry = document.querySelector("#blog-title").value;
   const blogEntry = document.querySelector("#blog-entry").value;
+  const titleShow = document.createElement("p");
+  const entryShow = document.createElement("p");
   const newWordCount = document.createElement("p");
   const newVowelCount = document.createElement("p");
   const newConsCount = document.createElement("p");
   const newTeaser = document.createElement("p");
   let newEntry = new Entry(titleEntry, blogEntry);
+
+  titleShow.innerHTML = "Title: " + titleEntry;
+  document.getElementById("response").append(titleShow);
+
+  entryShow.innerHTML = "Journal Entry: " + blogEntry;
+  document.getElementById("response").append(entryShow);
 
   newWordCount.innerHTML = "Word count: " + newEntry.countWords();
   document.getElementById("response").append(newWordCount);
@@ -26,6 +34,7 @@ function handleBlogEntry(event) {
 
   newTeaser.innerHTML = "Teaser: " + newEntry.getTeaser();
   document.getElementById("response").append(newTeaser);
+
 
 }
 
