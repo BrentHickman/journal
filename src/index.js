331c28400b94cb18c31cd0b971d1b6edffaf75e6
Brent <brenthickman@ymail.com>
2023-01-30 14:39:55 -0800

    ui logic
    Co-authored-by: johnedisc <christopher.johnedis@gmail.com>
    Co-Authored-By: Zookerman3 <szook7@gmail.com>

--- src/index.js
+++ src/index.js
@@ -9,8 +9,24 @@ function handleBlogEntry(event) {
   event.preventDefault();
   const titleEntry = document.querySelector("#blog-title").value;
   const blogEntry = document.querySelector("#blog-entry").value;
+  const newWordCount = document.createElement("p");
+  const newVowelCount = document.createElement("p");
+  const newConsCount = document.createElement("p");
+  const newTeaser = document.createElement("p");
   let newEntry = new Entry(titleEntry, blogEntry);
-  console.log(newEntry.getTeaser());
+
+  newWordCount.innerHTML = "Word count: " + newEntry.countWords();
+  document.getElementById("response").append(newWordCount);
+
+  newVowelCount.innerHTML = "Vowel count: " + newEntry.countVowels();
+  document.getElementById("response").append(newVowelCount);
+
+  newConsCount.innerHTML = "Consonant count: " + newEntry.countConsonants();
+  document.getElementById("response").append(newConsCount);
+
+  newTeaser.innerHTML = "Teaser: " + newEntry.getTeaser();
+  document.getElementById("response").append(newTeaser);
+
 }
 
 window.addEventListener("load", function() {
